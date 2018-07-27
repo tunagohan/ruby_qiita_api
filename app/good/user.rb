4b5e2abe53faec5e816c1fbddf11eec73c88a174
k-waragai <kazuxtuna@gmail.com>
2018-07-28 02:46:25 +0900

    use ok

--- app/good/user.rb
+++ app/good/user.rb
@@ -6,29 +6,23 @@ require 'pp'
 
 connect = Faraday.new(url: 'https://qiita.com/api/v2/items/')
 page_num = 1
-count = -1
+count = 1
 
 while count != 0
   response = connect.get "a372800c262f56fe688a/likes?page=#{page_num}"
+  page_num += 1
 
   body = JSON.parse(response.body)
+  count_amari = body.length % 20
 
-  count = body.length
-  p "---------------------"
-  p count
-  p "---------------------"
-
-  if count == 20
+  if count_amari == 0
     for i in 0..19 do
       p body[i]["user"]["id"]
     end
-  elsif (count % 20) > 1
-    for i in 0..(count % 20 ) -2 do
+  else count_amari
+    for i in 0..(count % 20 ) do
       p body[i]["user"]["id"]
     end
-  else
-    "終了"
+    break
   end
-
-  page_num += 1
 end
