a2c4cfe67ce484dfe89629b15ae16bd085e8cca9
k-waragai <kazuxtuna@gmail.com>
2018-07-28 02:28:58 +0900

    user ng

--- app/good/user.rb
+++ app/good/user.rb
@@ -6,25 +6,29 @@ require 'pp'
 
 connect = Faraday.new(url: 'https://qiita.com/api/v2/items/')
 page_num = 1
+count = -1
+
 while count != 0
-  response = connect.get "a372800c262f56fe688a/likes?pange=#{page_num}"
+  response = connect.get "a372800c262f56fe688a/likes?page=#{page_num}"
 
   body = JSON.parse(response.body)
-  headers = response.headers
-  link = headers["link"]
 
   count = body.length
-  if count  == 20
+  p "---------------------"
+  p count
+  p "---------------------"
+
+  if count == 20
     for i in 0..19 do
       p body[i]["user"]["id"]
     end
-  else
-    count = count % 20
-    for i in 0..count do
+  elsif (count % 20) > 1
+    for i in 0..(count % 20) do
       p body[i]["user"]["id"]
     end
+  else
+    "終了"
   end
 
   page_num += 1
-
 end
