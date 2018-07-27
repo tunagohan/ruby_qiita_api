423d0919e578630a5971a6522ab275ad3510ddd6
k-waragai <kazuxtuna@gmail.com>
2018-07-28 02:07:10 +0900

    user ng

--- app/good/user.rb
+++ app/good/user.rb
@@ -8,7 +8,18 @@ connect = Faraday.new(url: 'https://qiita.com/api/v2/items/')
 response = connect.get 'a372800c262f56fe688a/likes'
 
 body = JSON.parse(response.body)
-headers = JSON.parse(response.headers)
+headers = response.headers
+link = headers["link"]
 
-# p body
-puts headers
+while body.length != nil
+  if body.length / 20 == 1
+    for i in 0..19 do
+      p body[i]["user"]["id"]
+    end
+  else
+    count = body.length % 20
+    for i in 0..count do
+      p body[i]["user"]["id"]
+    end
+  end
+end
