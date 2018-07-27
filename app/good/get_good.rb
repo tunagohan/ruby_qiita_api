608c2bdeff938911cfcd292f39fc01f9c4d3d0fd
k-waragai <kazuxtuna@gmail.com>
2018-07-28 01:19:11 +0900

    pager

--- app/good/get_good.rb
+++ app/good/get_good.rb
@@ -3,11 +3,9 @@ require 'faraday'
 require 'json'
 require 'pp'
 
-
 conn = Faraday.new(:url => 'https://qiita.com/api/v2/items/')
-response = conn.get 'a372800c262f56fe688a/likes'
+response = conn.get 'a372800c262f56fe688a/likes?page=1'
+body = JSON.parse(response.body)
 
-# p response.headers
-
-user =['name','age']
-user = ['name']['age']
+p body
+# いいねしたユーザー数
