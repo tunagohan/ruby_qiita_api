30253c2ab82e5aa999de2207de5056221d8f1baf
RiesSleep <novsleep@outlook.jp>
2018-07-28 00:06:26 +0900

    cur Faraday

--- app/good/get_good.rb
+++ app/good/get_good.rb
@@ -1,18 +1,4 @@
-require 'faraday_curl'
-require 'logger'
 
-domain = "https://qiita.com/"
-logger = Logger.new(nil)
+require 'Faraday'
 
-connect =  Faraday.new(url: domain) do |f|
-  f.request :url_encoded
-  f.request :curl, logger, :warn
-  f.adapter Faraday.default_adapter
-end
-
-response = connect.get do |req|
-  req.url "/get"
-  req.headers['Authorization'] = auth if @use_get_token
-end
-
-p response.env[:curl_command]
+response = Faraday.get 'http://sushi.com/nigiri/sake.json'
